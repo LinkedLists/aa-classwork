a11a138db0cb5c7d8e34fd08ccfd81f7551ae9fa
appkenny <kzeng.95@gmail.com>
2020-12-04 12:05:54 -0800

    Switched after 15min

--- w3d5/skeleton 2/lib/00_tree_node.rb
+++ w3d5/skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
 
+    def parent=(value)
+        if value != nil
+            @parent = value
+        else
+            @parent = nil
+        end
+        
+        if !@parent.children.include?(self)
+            @parent.children << self
+        end
+
+    end
 end
