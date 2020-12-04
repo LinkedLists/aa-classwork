bad46eee18149cc124496754ec74ca3ef9fa628d
appkenny <kzeng.95@gmail.com>
2020-12-04 14:02:22 -0800

    Worked on parent

--- w3d5/skeleton 2/lib/00_tree_node.rb
+++ w3d5/skeleton 2/lib/00_tree_node.rb
@@ -3,19 +3,21 @@ require "byebug"
 class PolyTreeNode
     attr_reader :parent, :children, :value
     def initialize(value)
-        @parent = parent
+        @parent = nil
         @children = []
         @value = value
     end
 
     def parent=(value)
         # debugger
+        # old_parent = self.parent
         if value != nil
+            # old_parent.children.pop(self)
             @parent = value
             if !@parent.children.include?(self)
                 @parent.children << self
             end
-        else
+        # else
             
         end
 
