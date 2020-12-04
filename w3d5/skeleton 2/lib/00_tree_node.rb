4b53aabdf9853af3d52a7da821d52115acac4b71
colineckert <colineckert10@gmail.com>
2020-12-04 13:45:28 -0800

    Reassign parent to self

--- w3d5/skeleton 2/lib/00_tree_node.rb
+++ w3d5/skeleton 2/lib/00_tree_node.rb
@@ -1,21 +1,29 @@
+require "byebug"
+
 class PolyTreeNode
     attr_reader :parent, :children, :value
     def initialize(value)
-        @parent = nil
+        @parent = parent
         @children = []
         @value = value
     end
 
     def parent=(value)
+        # debugger
         if value != nil
             @parent = value
+            if !@parent.children.include?(self)
+                @parent.children << self
+            end
         else
-            @parent = nil
-        end
-        
-        if !@parent.children.include?(self)
-            @parent.children << self
+            
         end
 
     end
+
+    
 end
+
+a = PolyTreeNode.new(1)
+b = PolyTreeNode.new(2)
+a.parent = nil
