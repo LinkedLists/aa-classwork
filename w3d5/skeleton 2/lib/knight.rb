e0086ac209be7a4962a3fb50eb2b6c24c22b3865
appkenny <kzeng.95@gmail.com>
2020-12-07 18:08:11 -0800

    testing

--- w3d5/skeleton 2/lib/knight.rb
+++ w3d5/skeleton 2/lib/knight.rb
@@ -6,39 +6,39 @@ require "byebug"
     #build_move_tree only used when #find_path is called
     #::valid_moves(pos) => returns an array, needs to check with @considered_positions
 
-class KnightPathFinder
+# class KnightPathFinder
 
-    self.valid_moves(pos)
-        debugger
-        x = pos[0]
-        y = pos[1]
+#     self.valid_moves(pos)
+#         debugger
+#         x = pos[0]
+#         y = pos[1]
 
-        possible_moves = [[2, 1], [1, 2], [-2, 1], [-2, -1], [-1, 2], [2, -1], [1, -2], [-1, -2]]
+#         possible_moves = [[2, 1], [1, 2], [-2, 1], [-2, -1], [-1, 2], [2, -1], [1, -2], [-1, -2]]
 
-        possible_moves.map! do |pair|
-            pair[0] += x
-            pair[1] += y
-            pair
-        end 
+#         possible_moves.map! do |pair|
+#             pair[0] += x
+#             pair[1] += y
+#             pair
+#         end 
 
-        possible_moves.select! do |pair|
-            (pair[0] >= 0 && pair[0] < 8) && (pair[1] >= 0 && pair[1] < 8)
-        end
-    end
+#         possible_moves.select! do |pair|
+#             (pair[0] >= 0 && pair[0] < 8) && (pair[1] >= 0 && pair[1] < 8)
+#         end
+#     end
 
-    def initialize(pos)
-        debugger
-        @root_node = PolyTreeNode.new(pos)
-        @considered_positions = [@root_node]
+#     def initialize(pos)
+#         debugger
+#         @root_node = PolyTreeNode.new(pos)
+#         @considered_positions = [@root_node]
 
-    end
+#     end
 
-    def new_moves_position(pos)
-        if !@considered_positions.include?(pos)
-    end
+#     def new_moves_position(pos)
+#         if !@considered_positions.include?(pos)
+#     end
 
-end
+# end
 
-test = KnightPathFinder.new([0, 0])
+# test = KnightPathFinder.new([0, 0])
 
-p test
+# p test
