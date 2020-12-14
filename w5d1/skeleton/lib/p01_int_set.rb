a109407281074346bd7fc5a6a7fc063fb3e6cecc
appkenny <kzeng.95@gmail.com>
2020-12-14 14:10:27 -0800

    started resizing

--- w5d1/skeleton/lib/p01_int_set.rb
+++ w5d1/skeleton/lib/p01_int_set.rb
@@ -1,5 +1,6 @@
 require "byebug"
 class MaxIntSet
+  attr_reader :store
   def initialize(max)
     @store = Array.new(max, false)
   end
@@ -13,6 +14,7 @@ class MaxIntSet
   end
 
   def remove(num)
+    store[num] = false
   end
 
   def include?(num)
@@ -33,21 +35,26 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -64,12 +71,14 @@ class ResizingIntSet
   end
 
   def insert(num)
+    self[num] << num if !include?(num)
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
