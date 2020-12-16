e073743ad24f76374f0a85cf178a2df72a8e4d11
appkenny <kzeng.95@gmail.com>
2020-12-16 14:26:46 -0800

    continued created tables

--- w5d3/import_db.sql
+++ w5d3/import_db.sql
@@ -1,28 +1,39 @@
 CREATE TABLE users(
-    fname TEXT;
-    lname TEXT;
-    user_id INTEGER;
-    qf_id INTEGER;
+    fname TEXT,
+    lname TEXT,
+    user_id INTEGER PRIMARY KEY,
+    -- qf_id INTEGER,
 
-    FOREIGN_KEY (qf_id) REFERENCES question_follows(id)
+    -- FOREIGN_KEY (qf_id) REFERENCES question_follows(id)
 );
 
 CREATE TABLE questions(
-    title TEXT;
-    body TEXT;
-    question_id INTEGER;
-    qf_id INTEGER;
+    title TEXT,
+    body TEXT,
+    question_id INTEGER PRIMARY KEY,
+    -- qf_id INTEGER,
 
-    FOREIGN_KEY (qf_id) REFERENCES question_follows(id)
+    -- FOREIGN_KEY (qf_id) REFERENCES question_follows(id)
 );
 
 CREATE TABLE question_follows(
-    id INTEGER;
-    q_id INTEGER;
-    u_id INTEGER;    
+    id INTEGER PRIMARY KEY,
+    q_id INTEGER,
+    u_id INTEGER,
+
+    FOREIGN_KEY (q_id) REFERENCES questions(question_id)  
+    FOREIGN_KEY (u_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE replies();
+CREATE TABLE replies(
+  question_id INTEGER NOT NULL,
+  reply_id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  body_text TEXT,
+  FOREIGN_KEY (reply_id) REFERENCES replies(reply_id),
+  FOREIGN_KEY (question_id) REFERENCES question_follows(q_id),
+  FOREIGN_KEY (user_id) REFERENCES question_follows(u_id),
+);
 
 CREATE TABLE question_likes();
 
