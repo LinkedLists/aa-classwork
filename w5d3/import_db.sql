2e151544adf0caf22209968bea104fc0d99bd209
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-16 14:49:31 -0800

    Finished initial database setup

--- w5d3/import_db.sql
+++ w5d3/import_db.sql
@@ -1,7 +1,16 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+
+
 CREATE TABLE users(
     fname TEXT,
     lname TEXT,
-    user_id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY
     -- qf_id INTEGER,
 
     -- FOREIGN_KEY (qf_id) REFERENCES question_follows(id)
@@ -10,7 +19,7 @@ CREATE TABLE users(
 CREATE TABLE questions(
     title TEXT,
     body TEXT,
-    question_id INTEGER PRIMARY KEY,
+    question_id INTEGER PRIMARY KEY
     -- qf_id INTEGER,
 
     -- FOREIGN_KEY (qf_id) REFERENCES question_follows(id)
@@ -21,19 +30,27 @@ CREATE TABLE question_follows(
     q_id INTEGER,
     u_id INTEGER,
 
-    FOREIGN_KEY (q_id) REFERENCES questions(question_id)  
-    FOREIGN_KEY (u_id) REFERENCES users(user_id)
+    FOREIGN KEY (q_id) REFERENCES questions(question_id),  
+    FOREIGN KEY (u_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE replies(
-  question_id INTEGER NOT NULL,
-  reply_id INTEGER PRIMARY KEY,
-  user_id INTEGER,
-  body_text TEXT,
-  FOREIGN_KEY (reply_id) REFERENCES replies(reply_id),
-  FOREIGN_KEY (question_id) REFERENCES question_follows(q_id),
-  FOREIGN_KEY (user_id) REFERENCES question_follows(u_id),
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER,
+    reply_id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    body_text TEXT,
+    FOREIGN KEY (parent_id) REFERENCES replies(reply_id),
+    FOREIGN KEY (question_id) REFERENCES questions(question_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE question_likes();
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY,
+    q_id INTEGER,
+    u_id INTEGER,
+
+    FOREIGN KEY (q_id) REFERENCES questions(question_id),  
+    FOREIGN KEY (u_id) REFERENCES users(user_id)
+);
 
