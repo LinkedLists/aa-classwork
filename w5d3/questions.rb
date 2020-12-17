f4f6171235b26bb00330c0cfca96e20495943537
appkenny <kzeng.95@gmail.com>
2020-12-16 16:02:01 -0800

    continued making classes for the tables

--- w5d3/questions.rb
+++ w5d3/questions.rb
@@ -37,3 +37,62 @@ class Users
     @lname = options['lname']
   end
 end
+
+class Question
+  attr_accessor :question_id, :body, :title, :author_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    question = PlayDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        question_id = ?
+    SQL
+    return nil unless question.length > 0
+    Question.new(question.first)
+  end
+
+  def initialize(options)
+    @question_id = options['question_id']
+    @body = options['body']
+    @title = options['title']
+    @author_id = options['author_id']
+  end
+end
+
+class QuestionFollows
+  attr_accessor :id, :q_id, :u_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM question_follows")
+    data.map { |datum| QuestionFollows.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    question_follow = PlayDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+    return nil unless question_follow.length > 0
+    QuestionFollows.new(question_follow.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @q_id = options['q_id']
+    @u_id = options['u_id']
+  end
+end
+
+class Replies
