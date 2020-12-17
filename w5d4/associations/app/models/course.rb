431c62a07a23cd835e0d752008f89024c09ecabc
Tului <tgantulga@ucdavis.edu>
2020-12-17 14:41:39 -0800

    got course and enrollment working

--- w5d4/associations/app/models/course.rb
+++ w5d4/associations/app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :id,
+        class_name: :Enrollment
+
 end
--- w5d4/associations/app/models/enrollment.rb
+++ w5d4/associations/app/models/enrollment.rb
@@ -1,2 +1,12 @@
 class Enrollment < ApplicationRecord
+
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+
+    
+
+# select * from User where student_id = user_id
+
 end
