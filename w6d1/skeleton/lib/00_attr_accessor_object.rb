b6247f2178a12b2b34d95e33f8eef04cd424e802
appkenny <kzeng.95@gmail.com>
2020-12-28 14:30:51 -0800

    finished 00

--- w6d1/skeleton/lib/00_attr_accessor_object.rb
+++ w6d1/skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,20 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+      define_method(name) do #lol define one at a time dude
+        self.instance_variable_get("@#{name}")
+        # def :@name=(arg)
+        #   @name = arg
+        # end
+        # def self.name
+        #   name
+        # end
+      end
+
+      define_method("#{name}=") { |arg| self.instance_variable_set("@#{name}", arg)} #need @ to refer to instance var???
+    end
   end
+
+
 end
