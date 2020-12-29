f40265caf550725b53980d462720e83c014a75d7
Mona Miga <monaleez@love.com>
2020-12-29 15:21:41 -0800

    add user model and table

--- w6d3/routes/app/controllers/users_controller.rb
+++ w6d3/routes/app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
   def index
-    # render json: User.all
     render plain: 'Hi'
   end
 
   def create
-    # users = User.new
     render json: params
   end
   
-  # def show
-  #   # user = User.find(params[:id])
-  #   render json: User.find(params[:id])
-  # end
+  def show
+    render json: params
+    # user = User.find(params[:id])
+    # render json: User.find(params[:id])
+  end
+
+
 end
