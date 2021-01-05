f9bbd74f0f461e6499428e3e3aedfb6be900e3b9
appkenny <kzeng.95@gmail.com>
2021-01-05 11:19:27 -0800

    finished user and session controllers

--- w7d2/music_app/app/controllers/users_controller.rb
+++ w7d2/music_app/app/controllers/users_controller.rb
@@ -7,5 +7,17 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(params.require(:user).permit(:email,:password)
+
+        if @user.save
+            login(@user) #login method from app. controller
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
     end
 end
