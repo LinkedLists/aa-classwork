7ec216380566899e768ef55b3c9c962fa0742fce
bcdguz <bcdguz96@gmail.com>
2021-01-05 11:02:35 -0800

    Starting to create userscontroller and sessionscontroller

--- w7d2/music_app/app/models/user.rb
+++ w7d2/music_app/app/models/user.rb
@@ -10,10 +10,21 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  attr_reader :password
   validates :email, presence: :true, uniqueness: :true
   validates :session_token, presence: :true
   validates :password_digest, presence: :true
+  validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      user
+    else  
+      nil
+    end
+  end
 
   def self.generate_session_token
     SecureRandom::urlsafe_base64
@@ -28,4 +39,15 @@ class User < ApplicationRecord
     self.save!
     self.session_token
   end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
 end
