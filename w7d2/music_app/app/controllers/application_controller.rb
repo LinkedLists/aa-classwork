d0100a5ed3a71bb915dab0775e2b63b8a14691af
bcdguz <bcdguz96@gmail.com>
2021-01-05 12:20:59 -0800

    Working on #destroy in sessions_controller

--- w7d2/music_app/app/controllers/application_controller.rb
+++ w7d2/music_app/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def current_user
-    @current_user = User.find_by(session_token: session[:session_token])
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def login!(user)
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def log_out
+    if logged_in?
+      current_user.reset_session_token!
+      session[:session_token] = nil
+      @current_user = nil
+    end
   end
 
 end
