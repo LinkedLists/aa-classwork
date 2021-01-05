89bbf49c851e407f7f02ffadc2494d9d22eb7506
Janette <tinoconjanette@gmail.com>
2021-01-04 16:56:52 -0800

    Added sessions new html

--- w7d1/cats_auth/app/controllers/sessions_controller.rb
+++ w7d1/cats_auth/app/controllers/sessions_controller.rb
@@ -7,15 +7,14 @@ class SessionsController < ApplicationController
     def create ##creates a new session 
         @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
 
-    if @user
-        #after user.save login the user
-        # @user.reset_session_token!
-        session[:session_token] = @user.reset_session_token!
-        redirect_to cats_url
-    else
-        render :new
+        if @user
+            #after user.save login the user
+            # @user.reset_session_token!
+            session[:session_token] = @user.reset_session_token!
+            redirect_to cats_url
+        else
+            render :new
+        end
     end
-  end
-
 
 end
