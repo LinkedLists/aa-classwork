d91911342041c86a2b7f69865e9da1b6439dc06f
appkenny <kzeng.95@gmail.com>
2021-01-05 14:33:40 -0800

    worked on band controller actions

--- w7d2/music_app/app/controllers/bands_controller.rb
+++ w7d2/music_app/app/controllers/bands_controller.rb
@@ -5,18 +5,50 @@ class BandsController < ApplicationController
     end
 
     def create
-        @user = User.new(params.require(:user).permit(:email,:password))
+        @band = Band.new(band_params)
 
-        if @user.save
-            login!(@user) #login method from app. controller
-            redirect_to user_url(@user)
+        if @band.save
+            redirect_to band_url(@band)
         else
             render :new
         end
     end
 
     def show
-        @user = User.find(params[:id])
+        @band = Band.find(params[:id])
         render :show
+    end
+
+    def index
+        @band = Band.all
+        render :index
+    end
+
+    def edit
+        @band = Band.find(params[:id])
+        render :edit
+    end
+    
+    def update
+        @band = Band.find(params[:id])
+
+        if @band.update(band_params)
+            redirect_to band_url(@band)
+        else
+            render :edit
+        end
+
+    end
+
+    def destroy
+        band = Band.find(params[:id])
+        band.destroy
+        redirect_to bands_url
+    end
+
+    private
+
+    def band_params
+        params.require(:band).permit(:name)
     end
 end
