8ea23d2f82359901c67cc8f73d17ebf7339a4b80
appkenny <kzeng.95@gmail.com>
2021-01-11 11:05:34 -0800

    finished sub controller and worked on sub views

--- w8d1/reddit_app/app/controllers/subs_controller.rb
+++ w8d1/reddit_app/app/controllers/subs_controller.rb
@@ -1,4 +1,7 @@
 class SubsController < ApplicationController
+    before_action :require_logged_in, only: [:new, :create]
+    before_action :require_is_moderator, only: [:update]
+
     def new
         @sub = Sub.new
         render :new
@@ -12,6 +15,43 @@ class SubsController < ApplicationController
         else
             flash.now[:errors] = @sub.errors.full_messages
             render :new
+        end
+    end
+
+    def edit
+        @sub = Sub.find_by_id(params[:id])
+        render :edit
+    end
+
+    def update
+        @sub = Sub.find_by_id(params[:id])
+        if @sub
+            if @sub.update(sub_params)
+                redirect_to sub_url(@sub)
+            else
+                flash.now[:errors] = @sub.errors.full_messages
+                render :edit
+            end
+        else
+            flash[:errors] = ["Sub could not be found"]
+            render :edit
+        end
+    end
+
+    def show
+        @sub = Sub.find_by_id(params[:id])
+        render :show
+    end
+
+    def index
+        @subs = Sub.all
+        render :index
+    end
+
+    def require_is_moderator
+        unless current_user.id == Sub.find_by_id(params[:id]).user_id
+            flash.now[:errors] = ["Sub can only be edited by the moderator"]
+            render :edit
         end
     end
 
--- w8d1/reddit_app/app/models/sub.rb
+++ w8d1/reddit_app/app/models/sub.rb
@@ -13,7 +13,7 @@ class Sub < ApplicationRecord
     validates :title, :description, :user_id, presence: true
     validates :title, uniqueness: true
 
-    belongs_to :user,
+    belongs_to :moderator,
         foreign_key: :user_id,
         class_name: :User
 end
