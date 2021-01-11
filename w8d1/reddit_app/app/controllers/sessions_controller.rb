eba2485d4db3649e633c6a8e99884dfc52526b75
appkenny <kzeng.95@gmail.com>
2021-01-11 10:23:32 -0800

    created views for session and user

--- w8d1/reddit_app/app/controllers/sessions_controller.rb
+++ w8d1/reddit_app/app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
-    
+  before_action :require_logged_in, only: [:destroy]
+
+  def new
+    render :new
+  end
+  
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if user
+      login!(user)
+      redirect_to new_user_url #fix later
+    else
+      flash[:errors] = ["Invalid credentials"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+  end
 end
