cbc831fed470e77833909088fe1e1a0efdbe5f6b
appkenny <kzeng.95@gmail.com>
2021-01-11 12:22:41 -0800

    worked on post actions and post views

--- w8d1/reddit_app/app/controllers/posts_controller.rb
+++ w8d1/reddit_app/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_is_author, only: [:update]
+  before_action :require_is_author, only: [:update, :destroy]
   before_action :require_logged_in, only: [:create, :update]
 
   def new
@@ -24,12 +24,35 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.new
+    render :edit
   end
 
   def update
+    @post = Post.find_by_id(params[:id])
+
+    if @post
+      if @post.update(post_params)
+        redirect_to post_url(@post)
+      else
+        flash.now[:errors] = @post.errors.full_messages
+        render :edit
+      end
+    else
+      flash[:errors] = ["Post can't be found"]
+      redirect_to subs_url
+    end
   end
 
   def destroy
+    post = Post.find_by_id(params[:id])
+
+    post.delete if post
+      
+      
+    # else
+    # end
+    redirect_to subs_url
   end
 
   def require_is_author
