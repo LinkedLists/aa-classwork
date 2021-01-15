89364ae48f835eebb67eafee6394bd62232c62be
annieyxu <annieyxu@berkeley.edu>
2021-01-15 14:51:25 -0800

    addNumbers

--- w8d5/callbacks.js
+++ w8d5/callbacks.js
@@ -11,7 +11,7 @@ class Clock {
         // 3. Call printTime.
         this.printTime(this.hours, this.minutes, this.seconds)
         // 4. Schedule the tick at 1 second intervals.
-        setInterval(this._tick,1000)
+        setInterval(this._tick.bind(this),1000)
     }
 
     printTime(hours, minutes, seconds) {
@@ -20,36 +20,39 @@ class Clock {
         // Use console.log to print it.
         console.log(`${hours}:${minutes}:${seconds}`)
     }
-
+    
     _tick() {
+        // console.log(this)
         // 1. Increment the time by one second.
         this._incrementSeconds();
         // 2. Call printTime.
         this.printTime(this.hours, this.minutes, this.seconds)
     }
-
+    
     _incrementSeconds() {
-      this.seconds++;
-      if (this.seconds === 60) {
-        this.minute = 0;
-        this._incrementMinutes();
-      }
+        this.seconds++;
+        if (this.seconds === 60) {
+            this.minute = 0;
+            this._incrementMinutes();
+        }
     }
-
+    
     _incrementMinutes() {
-      this.minute++;
-      if (this.minute === 60) {
-        this.minute = 0;
-        this._incrementHours();
-      }
+        this.minute++;
+        if (this.minute === 60) {
+            this.minute = 0;
+            this._incrementHours();
+        }
     }
-
+    
     _incrementHours() {
-      this.hours++;
-      if (this.hours === 24) {
-        this.hours = 0;
-      }
+        this.hours++;
+        if (this.hours === 24) {
+            this.hours = 0;
+        }
     }
 }
 
 const clock = new Clock();
+
+function addNumbers(sum, numLeft, completionCallback)
