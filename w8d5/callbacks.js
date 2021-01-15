47cf39236b6d63a5fcf1b97a5757fef0901869a7
appkenny <kzeng.95@gmail.com>
2021-01-15 15:06:38 -0800

    compeleted addNumber

--- w8d5/callbacks.js
+++ w8d5/callbacks.js
@@ -53,6 +53,31 @@ class Clock {
     }
 }
 
-const clock = new Clock();
+// const clock = new Clock();
 
-function addNumbers(sum, numLeft, completionCallback)
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  // it's okay if this part is magic; it just says that we want to
+  // 1. output the prompt to the standard output (console)
+  // 2. read input from the standard input (again, console)
+
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numLeft, completionCallback) {
+  
+  if (numLeft > 0) {
+    reader.question("What is your number?", function (answer) {
+      let num = parseInt(answer);
+      sum += num;
+      console.log(`Partial sum ${sum}!`);
+      addNumbers(sum, numLeft - 1, completionCallback)
+    });
+  } else {
+    completionCallback(sum)
+  }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
