f1506ac73ad15c0747f1cc78272cf0f5fe3442b0
Brandon <estanob@gmail.com>
2021-02-04 15:40:43 -0800

    Begin user auth

--- w11d4/benchBnb/app/controllers/api/users_controller.rb
+++ w11d4/benchBnb/app/controllers/api/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::UsersController < ApplicationController
+  
+  def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+
+    if @user.save!
+      # login user (in application controller)
+      render json: @user
+    end
+  end
+
+  def show
+    @user = User.find_by(params[:id])
+    render json: @user
+  end 
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
 end
--- w11d4/benchBnb/app/controllers/application_controller.rb
+++ w11d4/benchBnb/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  #CRLLL
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+  def logged_in?
+  end
+
 end
