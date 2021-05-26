5ba27a10c9fd7eb41b78eebfea947addf487c11f
appkenny <kzeng.95@gmail.com>
2021-05-25 21:27:55 -0700

    completed tree height and sum

--- w16d2/depth_breadth_first_project/lib/tree_sum.js
+++ w16d2/depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,15 @@
 function treeSum(root) {
+    // let sum = 0
 
+    // if (!root) return sum
+
+    // sum += treeSum(root.left) + treeSum(root.right)
+
+    // return sum
+
+    if (!root) return 0;
+
+    return treeSum(root.left) + root.val + treeSum(root.right);
 }
 
 
