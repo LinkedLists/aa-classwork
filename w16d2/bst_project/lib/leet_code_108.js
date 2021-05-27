a0dcfa9231b2c5ff8dad7f1508266fd5782a24bd
appkenny <kzeng.95@gmail.com>
2021-05-26 20:18:05 -0700

    finished leetcode 108

--- w16d2/bst_project/lib/leet_code_108.js
+++ w16d2/bst_project/lib/leet_code_108.js
@@ -3,5 +3,17 @@
 
 
 function sortedArrayToBST(nums) {
+  if (!nums.length) return null;
+    
+  let i = Math.floor(nums.length / 2);
 
+  let root = new TreeNode(nums[i]);
+
+  let left = sortedArrayToBST(nums.slice(0, i))
+  let right = sortedArrayToBST(nums.slice(i + 1))
+
+  root.left = left
+  root.right = right
+
+  return root
 }
