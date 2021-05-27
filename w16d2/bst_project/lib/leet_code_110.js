614925e2fefd0aa63370abc20099078d237a8f30
appkenny <kzeng.95@gmail.com>
2021-05-26 20:46:12 -0700

    finished leetcode 110

--- w16d2/bst_project/lib/leet_code_110.js
+++ w16d2/bst_project/lib/leet_code_110.js
@@ -3,5 +3,15 @@
 
 
 function isBalanced(root) {
+  if (!root) return false
 
+  let balanced = Math.abs(getHeight(root.left) - getHeight(root.right))
+
+  return balanced <= 1 && isBalanced(root.left) && isBalanced(root.right)
+}
+
+function getHeight(root) {
+  if (!root) return -1;
+
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
 }
