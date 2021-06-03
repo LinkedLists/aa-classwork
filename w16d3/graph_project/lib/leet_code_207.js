3cf3696bdf8233f3d7a95bfb20a88a05c50cfb47
appkenny <kzeng.95@gmail.com>
2021-06-02 22:48:06 -0700

    completed leetcode 207

--- w16d3/graph_project/lib/leet_code_207.js
+++ w16d3/graph_project/lib/leet_code_207.js
@@ -2,5 +2,47 @@
 //  https://leetcode.com/problems/course-schedule/
 
 function canFinish(numCourses, prerequisites) {
+  let prereq = buildGraph(prerequisites)
+  let completed = new Set()
+  let totalCourses = Object.keys(prereq).length
 
+  let eligible = true
+
+  while(eligible) {  
+    eligible = false  
+    for (let course in prereq) {
+       let prereqMet = prereq[course].every( pre => 
+         completed.has(pre)
+       )
+
+       if (!completed.has(course) && prereqMet) {
+         completed.add(course)
+         eligible = true
+       }
+    }
+  }
+
+  return completed.size === totalCourses
+}
+
+function buildGraph(prereq) {
+  let graph = {}
+
+  prereq.forEach( pair => {
+    let [course, pre] = pair.map(String)
+
+    if (course in graph) {
+      graph[course].push(pre)
+    } else {
+      graph[course] = [pre]
+    }
+
+    if (!(pre in graph)) {
+      graph[pre] = []
+    }
+
+
+  })
+
+  return graph
 }
