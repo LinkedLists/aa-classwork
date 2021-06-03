62067ccc80504c06df4e9123bf713b1c25606070
appkenny <kzeng.95@gmail.com>
2021-06-02 21:03:34 -0700

    completed maxValue and numRegions

--- w16d3/graph_project/lib/max_value.js
+++ w16d3/graph_project/lib/max_value.js
@@ -1,5 +1,18 @@
 function maxValue(node, visited=new Set()) {
+    let queue = [node];
+    let max = 0
+    // let visited = new Set();
 
+    while (queue.length) {
+        let node = queue.shift();
+        if (visited.has(node)) continue;
+        visited.add(node);
+        if (node.val > max) max = node.val;
+
+        queue.push(...node.neighbors);
+    }
+
+    return max;
 }
 
 module.exports = {
